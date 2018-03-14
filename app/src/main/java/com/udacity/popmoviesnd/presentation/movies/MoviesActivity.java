a0ee50d3acce2ee5829f2ae7d1c224a4454f2b4a
Jose Lopez <josemiguel.lopez@kbc.be>
2018-03-14 18:59:19 +0100

    Added menu for the Movies activity

--- app/src/main/java/com/udacity/popmoviesnd/presentation/movies/MoviesActivity.java
+++ app/src/main/java/com/udacity/popmoviesnd/presentation/movies/MoviesActivity.java
@@ -2,6 +2,9 @@ package com.udacity.popmoviesnd.presentation.movies;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.udacity.popmoviesnd.R;
 import com.udacity.popmoviesnd.presentation.BaseActivity;
@@ -18,6 +21,22 @@ public class MoviesActivity extends BaseActivity
         setContentView(R.layout.activity_movies);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_movie_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        if (R.id.action_settings == item.getItemId()) {
+            startSettingsActivity();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
     @NonNull
     @Override
     protected MoviesActivityPresenter getPresenter() {
@@ -25,6 +44,11 @@ public class MoviesActivity extends BaseActivity
             presenter = new MoviesActivityPresenterImp();
         }
         return presenter;
+    }
+
+    private void startSettingsActivity() {
+        // TODO Add settings activity
+        Toast.makeText(this, "Go to Settings", Toast.LENGTH_SHORT).show();
     }
 
 }
