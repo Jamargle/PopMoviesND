52b2269440a5d1afe57f9459da1386a7481bfd8c
Jose Lopez <josemiguel.lopez@kbc.be>
2018-03-18 21:02:21 +0100

    Removed useless so far MVP stuff in DetailActivity

--- app/src/main/java/com/udacity/popmoviesnd/presentation/details/DetailActivity.java
+++ app/src/main/java/com/udacity/popmoviesnd/presentation/details/DetailActivity.java
@@ -1,20 +1,16 @@
 package com.udacity.popmoviesnd.presentation.details;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 
 import com.udacity.popmoviesnd.R;
 import com.udacity.popmoviesnd.domain.model.Movie;
-import com.udacity.popmoviesnd.presentation.BaseActivity;
 
-public final class DetailActivity extends BaseActivity
-        implements MovieDetailFragment.Callback,
-        DetailActivityPresenter.DetailActivityView {
+public final class DetailActivity extends AppCompatActivity
+        implements MovieDetailFragment.Callback {
 
     private static final String MOVIE_TO_SHOW = "param:movie_to_show";
-
-    private DetailActivityPresenter presenter;
 
     public static Bundle newBundle(final Movie movieToShow) {
         final Bundle bundle = new Bundle();
@@ -39,15 +35,6 @@ public final class DetailActivity extends BaseActivity
         super.onStart();
         final Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.movie_details_fragment);
         ((MovieDetailFragment) fragment).setMovieToShow((Movie) getIntent().getParcelableExtra(MOVIE_TO_SHOW));
-    }
-
-    @NonNull
-    @Override
-    protected DetailActivityPresenter getPresenter() {
-        if (presenter == null) {
-            presenter = new DetailActivityPresenterImp();
-        }
-        return presenter;
     }
 
 }
--- app/src/main/java/com/udacity/popmoviesnd/presentation/details/DetailActivityPresenterImp.java
+++
@@ -1,8 +0,0 @@
-package com.udacity.popmoviesnd.presentation.details;
-
-import com.udacity.popmoviesnd.presentation.BasePresenterImpl;
-
-public final class DetailActivityPresenterImp extends BasePresenterImpl<DetailActivityPresenter.DetailActivityView>
-        implements DetailActivityPresenter {
-
-}
